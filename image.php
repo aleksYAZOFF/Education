3a0700ee5ec16b85bf4d12021a7bc1e88cedd5b3
aleksYAZOFF <aleks.yazoff@gmail.com>
2021-09-30 23:46:31 +0500

    Third chapter

---
+++ image.php
@@ -0,0 +1,19 @@
+<?php
+
+	function Add_image($url, $alt=null, $height = null, $width = null){
+		$html = print '<img src="'.$url.'" alt="'.$alt.'" height="'.$height.'" widht="'.$widht.'"></img>';
+		return $html;
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
